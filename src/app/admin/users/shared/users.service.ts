53259eade0a77e35945e07db944b660924f19636
Arturas33 <jasinskis330@gmail.com>
2017-09-14 14:44:04 +0300

    user optimize structur

---
+++ src/app/admin/users/shared/users.service.ts
@@ -0,0 +1,59 @@
+import {Injectable} from '@angular/core';
+import {Http, Response, Headers} from '@angular/http';
+import 'rxjs/Rx';
+import {Observable} from 'rxjs';
+import {AuthService} from '../../auth.service';
+import {User} from './user';
+
+
+
+@Injectable()
+export class UsersService {
+    constructor(private http: Http, private authService: AuthService) {
+
+    }
+
+    getUsers(): Observable<any> {
+        const token = this.authService.getToken();
+        return this.http.get('http://becms.dev/api/users?token=' + token).map((response: Response) => {
+            return response.json().users;
+        });
+    }
+
+    createUser(user) {
+        const token = this.authService.getToken();
+        return this.http.post('http://becms.dev/api/users?token=' + token,
+            user,
+            {headers: new Headers({'X-Requested-With': 'XMLHttpRequest'})}
+        ).map(
+            (response: Response) => {
+                return true;
+            }
+        );
+    }
+
+    updateUser(user) {
+        const token = this.authService.getToken();
+        return this.http.put('http://becms.dev/api/users/' + user.id + '?token=' + token,
+            user,
+            {headers: new Headers({'Content-type': 'application/json'})}
+        ).map(
+            (response: Response) => response.json()
+        );
+    }
+
+    deleteUser(id: any) {
+        const token = this.authService.getToken();
+        return this.http.delete('http://becms.dev/api/users/' + id + '?token=' + token);
+    }
+
+    getUser(id: any): Observable<any> {
+        const token = this.authService.getToken();
+        return this.http.get('http://becms.dev/api/users/' + id + '?token=' + token)
+            .map(
+                (response: Response) => {
+                    return response.json().user;
+                }
+            );
+    }
+}
--- src/app/post.services.ts
+++ src/app/post.services.ts
@@ -1,8 +1,10 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
+import {Http, Response, Headers} from '@angular/http';
 import 'rxjs/Rx';
 import {Observable} from 'rxjs';
-import {AuthService} from './auth.service';
+import {AuthService} from './admin/auth.service';
+import {Post} from './post.interface';
+
 
 
 @Injectable()
@@ -13,8 +15,53 @@ export class PostService {
 
     getPosts(): Observable<any> {
         const token = this.authService.getToken();
-        return this.http.get('http://becms.dev/api/posts?token=' +token).map((response: Response) => {
+        return this.http.get('http://becms.dev/api/posts?token=' + token).map((response: Response) => {
             return response.json().posts;
         });
     }
+
+    createPost(user_id: string,
+               title: string,
+               text: string) {
+        const token = this.authService.getToken();
+        return this.http.post('http://becms.dev/api/posts?token=' + token,
+            {
+                user_id: user_id,
+                title: title,
+                text: text,
+
+            },
+            {headers: new Headers({'X-Requested-With': 'XMLHttpRequest'})}
+        ).map(
+            (response: Response) => {
+                return true;
+            }
+        );
+    }
+
+    getPost(id: any): Observable<any> {
+        const token = this.authService.getToken();
+        return this.http.get('http://becms.dev/api/posts/' + id + '?token=' + token)
+            .map(
+                (response: Response) => {
+                    return response.json().post;
+                }
+            );
+    }
+
+    updatePost(post: Post) {
+        const token = this.authService.getToken();
+        return this.http.put('http://becms.dev/api/posts/' + post.id + '?token=' + token,
+            JSON.stringify(post),
+            {headers: new Headers({'Content-type': 'application/json'})}
+        ).map(
+            (response: Response) => response.json()
+        );
+    }
+
+    deletePost(id: any) {
+        const token = this.authService.getToken();
+        return this.http.delete('http://becms.dev/api/posts/' + id + '?token=' + token);
+    }
+
 }
